1c6c4c6402dc4df696f525d618beaea5f9427042
jjang-occ <54513815+jjang-occ@users.noreply.github.com>
2023-11-30 19:18:17 -0800

    Update demo220.cpp remotely

--- demo220.cpp
+++ demo220.cpp
@@ -6,5 +6,6 @@ int main()
   cout << "Hello, Justin." << endl;
   cout << "A local change." << endl;
   cout << "Another local change." << endl;
+  cout << "A remote change." << endl;
   return 0;
 }
